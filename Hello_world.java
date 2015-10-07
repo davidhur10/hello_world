531d5e84b898ce1046579c2b62df362a040551b6
davidhur10 <davidhur10@hotmail.es>
2015-10-07 19:02:34 +0200

    canvi filosof

--- Hello_world.java
+++ Hello_world.java
@@ -15,7 +15,7 @@ public class Hello_world {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World") ;
+        System.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT") ;
     }
     
 }
