0527e63270212386c06be9fda0dd6d82887ab913
davidhur10 <davidhur10@hotmail.es>
2015-10-07 19:51:14 +0200

    vilanova

--- Hello_world.java
+++ Hello_world.java
@@ -15,7 +15,7 @@ public class Hello_world {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World") ;
+        System.out.println("Hello, World from Vilanova") ;
     }
     
 }
